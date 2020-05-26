0beb35928611dcbce5f144529976343fde2d2a7d
Jun M <jun.tomagan@secuna.io>
2020-05-26 20:44:39 +0800

    add page view

--- lib/simple_otp_web/views/page_view.ex
+++ lib/simple_otp_web/views/page_view.ex
@@ -1,22 +1,44 @@
 defmodule SimpleOtpWeb.PageView do
   use SimpleOtpWeb, :view
 
-  # this module is for testing randomly generated qr_code
+  def sort_vaults(vaults) do
+    vaults
+    |> Stream.chunk_every(3)
+    |> Enum.to_list()
+  end
 
-  def generate_qr_code() do
-    require Logger;
+  def sort_shells(shells) do
+    shells
+    |> Stream.chunk_every(3)
+    |> Enum.to_list()
+  end
 
-    otp = "totp"
-    app_name = "Simpleotp"
-    app_slug = "Simpleotp"
-    email = "x123x@mailinator.com" |> String.replace("@", "%40")
-    secret = OneTimePassEcto.Base.gen_secret(32)
+  def icon(conn, filename) do
+    Routes.static_path(conn, "/images/#{filename}")
+  end
 
-    qr_uri = "otpauth://#{otp}/#{app_name}:#{email}?secret=#{secret}"
-      <>"&issuer=#{app_slug}&algorithm=SHA1&digits=6&period=30"
+  def render_totp(secret) do
+    :pot.totp(secret)
+  end
 
-    qr_uri
+  def generate_qr(shell) do
+    shell
+    |> generate_uri()
     |> EQRCode.encode()
-    |> EQRCode.svg(color: "#cd07e3", width: 300)
+    |> EQRCode.svg(color: "#0d92c6", shape: "circle", width: 230)
+  end
+
+  def generate_uri(%{email: email, name: name, secret: secret})
+   when not is_nil(email) do
+    "otpauth://totp/#{name}:#{name}%20(#{email})?secret=#{secret}"
+  end
+
+  def generate_uri(%{username: username, name: name, secret: secret})
+   when not is_nil(username) do
+    "otpauth://totp/#{name}:#{name}%20(#{username})?secret=#{secret}"
+  end
+
+  def generate_uri(%{name: name, secret: secret}) do
+    "otpauth://totp/#{name}:#{name}?secret=#{secret}"
   end
 end
